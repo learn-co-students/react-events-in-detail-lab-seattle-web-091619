fe9a035c79eb0fea9273b2724d23311d5c17fa5e
Soumya_Goyal <somyagoyal739@gmail.com>
2019-11-20 23:33:02 -0800

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -22,15 +22,6 @@ export class DelayedButton extends Component {
     }
 }
 
-// handleClick = event => {
-//     event.persist();
-//     setTimeout(() => {
-//       this.props.onDelayedClick(event);
-//     }, this.props.delay);
-//   };
 
-//   render() {
-//     return <button onClick={this.handleClick}>Delayed</button>;
-//   }
 
 export default DelayedButton
